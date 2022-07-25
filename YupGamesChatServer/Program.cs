545b86a1350654d33a06e77199c2975feef12f1f
YupCore <k.gavrish07@gmail.com>
2022-07-25 18:15:14 +0300

    First commit

---
+++ YupGamesChatServer/Program.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace YupGamesChatServer
+{
+    public static class ClientsList
+    {
+        public static List<Client> clients = new List<Client>();
+    }
+
+    struct HandshakeStruct
+    {
+        public long clientUID;
+        public float gameVersion;
+        public string passwordHash;
+        public string roomName;
+    };
+
+    public class Client
+    {
+        HandshakeStruct handshakeStruct;
+        bool handshakeSuccsesful = false;
+        TcpClient theClient;
+        DateTime connectionDate;
+        public string GetMD5FromString(string toGet)
+        {
+            using(var md5 = MD5.Create())
+            {
+                var hashB = md5.ComputeHash(Encoding.UTF8.GetBytes(toGet));
+                return BitConverter.ToString(hashB);
+            }
+        }
+        public void WriteNewMessage(string message)
+        {
+            if(string.IsNullOrEmpty(message) || string.IsNullOrWhiteSpace(message))
+            {
+                return;
+            }
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+            List<byte> vs = new List<byte>();
+            for (int i = 0; i < 1024; i++)
+            {
+                vs.Add(255);
+            }
+
+            for (int i = 0; i < msg.Length; i++)
+            {
+                vs[i] = msg[i];
+            }
+
+            theClient.GetStream().Write(vs.ToArray(), 0, 1024);
+            WriteLog("writing a message " + message);
+        }
+        void WriteLog(string msg)
+        {
+            var formatted = "[" + DateTime.Now.ToString() + "]:" + msg + "\r\n";
+            var path = ("./logs/" + theClient.Client.RemoteEndPoint.ToString() + " : " + connectionDate.ToString()).Replace(':', '-') + ".log";
+            Console.Write(formatted);
+            if(!File.Exists(path))
+            {
+                using (var fs = File.Create(path))
+                {
+                    fs.Write(Encoding.UTF8.GetBytes(formatted));
+                }
+            }
+            else
+            {
+                File.AppendAllText(path, formatted);
+            }
+        }
+        public Client(TcpClient Client)
+        {
+            try
+            {
+                connectionDate = DateTime.Now;
+                theClient = Client;
+                WriteLog("new connection from " + ((IPEndPoint)Client.Client.RemoteEndPoint).Address.ToString());
+                // Буфер для хранения принятых от клиента данных
+                // Переменная для хранения количества байт, принятых от клиента
+                // Читаем из потока клиента до тех пор, пока от него поступают данные
+
+                WriteLog("receiving");
+                ClientsList.clients.Add(this);
+
+                string content_str2 = "";
+                try
+                {
+                    while(!Client.GetStream().DataAvailable) { }
+                    byte[] buffer2 = new byte[1024];
+
+                    Client.GetStream().Read(buffer2,0,1024);
+                    List<byte> trueBytes2 = new List<byte>();
+                    foreach (var b in buffer2)
+                    {
+                        if (b != 255)
+                        {
+                            trueBytes2.Add(b);
+                        }
+                    }
+                    content_str2 = Encoding.UTF8.GetString(trueBytes2.ToArray());
+                }
+                catch
+                {
+                    if (Client.Connected)
+                    {
+                        Client.Close();
+                    }
+                }
+                if (content_str2.Contains("HANDSHAKE>"))
+                {
+                    var hnds = JsonConvert.DeserializeObject<HandshakeStruct>(content_str2.Split('>')[1]);
+                    float gameVer;
+                    using (var wc = new WebClient())
+                    {
+                        gameVer = float.Parse(wc.DownloadString("https://yupgamesfiles.ddns.net/gamever.txt"));
+                    }
+                    if (hnds.gameVersion == gameVer)
+                    {
+                        if (hnds.passwordHash == GetMD5FromString("YGSHUI1937GH01@^)&!"))
+                        {
+                            handshakeSuccsesful = true;
+                            byte[] resp = Encoding.UTF8.GetBytes("HANDSHAKE SUCCESSFUL");
+                            List<byte> vs = new List<byte>();
+                            for (int i = 0; i < 1024; i++)
+                            {
+                                vs.Add(255);
+                            }
+
+                            for (int i = 0; i < resp.Length; i++)
+                            {
+                                vs[i] = resp[i];
+                            }
+
+                            Client.GetStream().Write(vs.ToArray(), 0, 1024);
+                            handshakeStruct = hnds;
+                            if (!ClientsList.clients.Contains(this))
+                            {
+                                ClientsList.clients.Add(this);
+                            }
+                        }
+                        else
+                        {
+                            handshakeSuccsesful = false;
+                            byte[] resp = Encoding.UTF8.GetBytes("HANDSHAKE FAILED : WRONG HASH");
+                            List<byte> vs = new List<byte>();
+                            for (int i = 0; i < 1024; i++)
+                            {
+                                vs.Add(255);
+                            }
+
+                            for (int i = 0; i < resp.Length; i++)
+                            {
+                                vs[i] = resp[i];
+                            }
+
+                            Client.GetStream().Write(vs.ToArray(), 0, 1024);
+                            Client.Close();
+                            ClientsList.clients.Remove(this);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        handshakeSuccsesful = false;
+                        byte[] resp = Encoding.UTF8.GetBytes("HANDSHAKE FAILED : WRONG VERSION");
+                        List<byte> vs = new List<byte>();
+                        for (int i = 0; i < 1024; i++)
+                        {
+                            vs.Add(255);
+                        }
+
+                        for (int i = 0; i < resp.Length; i++)
+                        {
+                            vs[i] = resp[i];
+                        }
+
+                        Client.GetStream().Write(vs.ToArray(), 0, 1024);
+                        Client.Close();
+                        ClientsList.clients.Remove(this);
+                        return;
+                    }
+                }
+                else
+                {
+                    handshakeSuccsesful = false;
+                    byte[] resp = Encoding.UTF8.GetBytes("HANDSHAKE FAILED : NO HANDSHAKE PROVIDED");
+                    List<byte> vs = new List<byte>();
+                    for (int i = 0; i < 1024; i++)
+                    {
+                        vs.Add(255);
+                    }
+
+                    for (int i = 0; i < resp.Length; i++)
+                    {
+                        vs[i] = resp[i];
+                    }
+                    WriteLog("Disconnecting");
+                    Client.GetStream().Write(vs.ToArray(), 0, 1024);
+                    Client.Close();
+                    ClientsList.clients.Remove(this);
+                    return;
+                }
+
+
+
+                while (Client.Connected)
+                {
+                    string content_str = "";
+                    try
+                    {
+                        byte[] buffer = new byte[1024];
+
+                        Client.GetStream().Read(buffer);
+                        List<byte> trueBytes = new List<byte>();
+                        foreach (var b in buffer)
+                        {
+                            if (b != 255)
+                            {
+                                trueBytes.Add(b);
+                            }
+                        }
+                        content_str = Encoding.UTF8.GetString(trueBytes.ToArray());
+                    }
+                    catch
+                    {
+                        if (Client.Connected)
+                        {
+                            Client.Close();
+                        }
+                        break;
+                    }
+
+                    try
+                    {
+                        if (!content_str.Contains("STPPOINT"))
+                        {
+                            if (handshakeSuccsesful)
+                            {
+                                foreach (var cl in ClientsList.clients)
+                                {
+                                    if (cl.handshakeStruct.roomName == this.handshakeStruct.roomName)
+                                    {
+                                        cl.WriteNewMessage(content_str);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        byte[] exByteString = Encoding.UTF8.GetBytes(ex.ToString());
+                        List<byte> vs = new List<byte>();
+                        for (int i = 0; i < 1024; i++)
+                        {
+                            vs.Add(255);
+                        }
+
+                        for (int i = 0; i < exByteString.Length; i++)
+                        {
+                            vs[i] = exByteString[i];
+                        }
+
+                        Client.GetStream().Write(vs.ToArray(), 0, 1024);
+                        continue;
+                    }
+                }
+                WriteLog("Disconnecting");
+                ClientsList.clients.Remove(this);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Unexpected behauvor: " + ex.Message + " \r\n Diconnecting");
+                if(this.theClient.Connected)
+                {
+                    theClient.Close();
+                }
+                ClientsList.clients.Remove(this);
+            }
+        }
+        ~Client()
+        {
+            ClientsList.clients.Remove(this);
+        }
+    }
+
+    class Server
+    {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+        public Server(int Port)
+        {
+            Listener = new TcpListener(IPAddress.Any, Port); // Создаем "слушателя" для указанного порта
+            Listener.Start(); // Запускаем его
+
+            // В бесконечном цикле
+            while (true)
+            {
+                try
+                {
+                    // Принимаем новых клиентов. После того, как клиент был принят, он передается в новый поток (ClientThread)
+                    // с использованием пула потоков.
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        static void ClientThread(Object StateInfo)
+        {
+            new Client((TcpClient)StateInfo);
+            // Просто создаем новый экземпляр класса Client и передаем ему приведенный к классу TcpClient объект StateInfo    
+        }
+
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Directory.CreateDirectory("./logs");
+                // Определим нужное максимальное количество потоков
+                // Пусть будет по 4 на каждый процессор
+                int MaxThreadsCount = Environment.ProcessorCount * 4;
+                // Установим максимальное количество рабочих потоков
+                ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+                // Установим минимальное количество рабочих потоков
+                ThreadPool.SetMinThreads(2, 2);
+                // Создадим новый сервер на порту 1420
+                new Server(1420);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,261 @@
+C# message exchange server, with handshakes for authentication with rooms system. Usage(unity):
+
+```cs
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using UnityEngine.SceneManagement;
+
+public struct HandshakeStruct
+{
+    public long clientUID; // no use at time
+    public float gameVersion; // used for my game version, you can delete it here and on server
+    public string passwordHash; // hash from password, not very secure lol)
+    public string roomName; //name says it
+    public HandshakeStruct(long uid,float ver,string hash, string roomName)
+    {
+        this.clientUID = uid;
+        this.gameVersion = ver;
+        this.passwordHash = hash;  
+        this.roomName = roomName;
+    }
+};
+
+public class ChatController : MonoBehaviour
+{
+    public TMPro.TMP_InputField chatInput;
+    public TMPro.TMP_InputField chat;
+    public static ChatController instance;
+    public GameObject chatCanvas;
+    public bool inRoom = false;
+
+    TcpClient theClient;
+    bool hndsSuccesful = false;
+
+    const float maxTimer = 4f;
+    float timer = maxTimer;
+
+    private string localChat = "";
+
+    private void Awake() // Some singleton stuff, don't look much at it
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static string GetMD5FromString(string toGet) // Used for auth
+    {
+        using (var md5 = MD5.Create())
+        {
+            var hashB = md5.ComputeHash(Encoding.UTF8.GetBytes(toGet));
+            return BitConverter.ToString(hashB);
+        }
+    }
+
+    public void Connect(HandshakeStruct structH) // write your struct
+    {
+        if(theClient == null)
+        {
+            theClient = new TcpClient("yourdomain or ip", 1420);
+            string conStr = "HANDSHAKE>"; //Separator that indicates a handshake
+
+            HandshakeStruct hs = structH;
+            conStr += JsonConvert.SerializeObject(hs); // Serializing a handshake using Newtonsoft.Json
+            byte[] hndsB = Encoding.UTF8.GetBytes(conStr); // converting all to bytes
+            List<byte> vs = new List<byte>(0);
+            for (int i = 0; i < 1024; i++) // for fixed size of 1KB
+            {
+                vs.Add(255);
+            }
+
+            for (int i = 0; i < hndsB.Length; i++)
+            {
+                vs[i] = hndsB[i];
+            }
+
+            theClient.GetStream().Write(vs.ToArray(), 0, vs.Count);
+            inRoom = true;
+            chatInput.gameObject.SetActive(true);
+            Debug.Log("Connected");
+        }
+    }
+
+    // Update is called once per frame
+    async void Update()
+    {
+        if (theClient != null)
+        {
+            try
+            {
+                byte[] buf = new byte[1024];
+                await theClient.GetStream().ReadAsync(buf, 0, buf.Length); // Fetching new messages from other clients
+
+                if (buf.Length > 0)
+                {
+                    List<byte> respBytes = new List<byte>();
+                    foreach (var b in buf)
+                    {
+                        if (b != 255)
+                        {
+                            respBytes.Add(b);
+                        }
+                    }
+
+                    string respString = Encoding.UTF8.GetString(respBytes.ToArray());
+                    if (respString.Contains("HANDSHAKE SUCCESSFUL") && !hndsSuccesful)
+                    {
+                        hndsSuccesful = true;
+                    }
+                    if (string.IsNullOrEmpty(respString) || string.IsNullOrWhiteSpace(respString))
+                    {
+                        return;
+                    }
+                    localChat += respString + "\r\n";
+                    timer = maxTimer;
+                    chatCanvas.SetActive(true);
+                    chat.text = localChat;
+                }
+            }
+            catch
+            {
+                if(theClient != null)
+                {
+                    theClient.Close();
+                    theClient = null;
+                }
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        if(inRoom)
+        {
+            if (Input.GetKeyDown(KeyCode.T) && !chatInput.isFocused)
+            {
+                timer = maxTimer;
+                if (SceneManager.GetActiveScene().name != "MainMeni")
+                {
+                    if (!chatInput.gameObject.activeInHierarchy)
+                    {
+                        chatCanvas.SetActive(!chatInput.gameObject.activeInHierarchy);
+                        chatInput.gameObject.SetActive(!chatInput.gameObject.activeInHierarchy);
+                        Cursor.lockState = CursorLockMode.None;
+                        Cursor.visible = true;
+                    }
+                    else
+                    {
+                        chatCanvas.SetActive(!chatInput.gameObject.activeInHierarchy);
+                        chatInput.gameObject.SetActive(!chatInput.gameObject.activeInHierarchy);
+                        Cursor.lockState = CursorLockMode.Locked;
+                        Cursor.visible = false;
+                    }
+                }
+                else
+                {
+                    chatCanvas.SetActive(!chatInput.gameObject.activeInHierarchy);
+                    chatInput.gameObject.SetActive(!chatInput.gameObject.activeInHierarchy);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                timer = maxTimer;
+                if (chatCanvas.activeInHierarchy)
+                {
+                    WriteMessage(chatInput);
+                    chatInput.text = "";
+                }
+            }
+            if (timer > 0f)
+            {
+                timer -= Time.deltaTime;
+            }
+            else if (!chatInput.isFocused)
+            {
+                chatInput.gameObject.SetActive(false);
+                chatCanvas.SetActive(false);
+                timer = maxTimer;
+            }
+        }
+        else
+        {
+            chatInput.gameObject.SetActive(false);
+            chatCanvas.SetActive(false);
+        }
+    }
+
+    public void WriteMessage(TMPro.TMP_InputField infield)
+    {
+        string message = "[" + Photon.Pun.PhotonNetwork.LocalPlayer.NickName + "]" + infield.text; //Formatting with nick ex. [Amogus]Sssss
+        if(theClient != null)
+        {
+            if (string.IsNullOrEmpty(infield.text) || string.IsNullOrWhiteSpace(infield.text) || message.Contains("STPPOINT")) 
+            {
+                return;
+            }
+            byte[] msbody = Encoding.UTF8.GetBytes(message); //encode the message
+            List<byte> vs = new List<byte>(1024);
+            for (int i = 0; i < 1024; i++)
+            {
+                vs.Add(255); // used as null characters, theese guys(ÿ) will be igonred so dont write them! Idk why i choose them you can change it
+            }
+
+            for (int i = 0; i < msbody.Length; i++)
+            {
+                vs[i] = msbody[i];
+            }
+
+            theClient.GetStream().Write(vs.ToArray(), 0, vs.Count);
+
+            byte[] msbody2 = Encoding.UTF8.GetBytes("STPPOINT"); // STP POINT is just like end character of message, it is written after every message
+            List<byte> vs2 = new List<byte>(1024);
+            for (int i = 0; i < 1024; i++)
+            {
+                vs2.Add(255);
+            }
+
+            for (int i = 0; i < msbody2.Length; i++)
+            {
+                vs2[i] = msbody2[i];
+            }
+
+            theClient.GetStream().Write(vs2.ToArray(), 0, vs2.Count);
+        }
+    }
+    public async void Disconnect()
+    {
+        if (theClient != null)
+        {
+            chatCanvas.SetActive(false);
+            localChat = "";
+            inRoom = false;
+            await Task.Delay(100);
+            theClient.Close();
+            theClient = null;
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        if(theClient != null)
+        {
+            theClient.Close();
+            theClient = null;
+        }
+    }
+}
+
+```
